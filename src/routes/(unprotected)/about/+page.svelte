0113dbac7a2ceacf8971f81a668fe283cab2aa11
Chris Allen Moore <chris@chrisallenmoore.com>
2022-10-07 09:37:22 +0700

    refactored a bit

---
+++ src/routes/(unprotected)/about/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<section class="page-px section-py">
+	<h1 class="text-3xl">About</h1>
+
+	{#if !$page.data.user}
+		<p>This is the about page.</p>
+		<p>This will show up only when a user IS NOT signed in.</p>
+	{/if}
+
+	{#if $page.data.user}
+		<p>Hi, {$page.data.user}. This is the about page.</p>
+		<p>This will show up only when a user IS signed in.</p>
+	{/if}
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 
 <section class="page-px section-py">
 	<h1 class="text-4xl">SvelteKit & Prisma with JWT Auth</h1>
-	<p class="text-2xl">This app demonstrates</p>
+	<p class="text-2xl">This app demonstrates how to set things up.</p>
 </section>
